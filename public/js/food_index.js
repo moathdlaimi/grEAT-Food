84fe97d846d2605edd70b7920625294f14a195d4
Moath <moath.dlaii@gmail.com>
2020-06-07 15:21:05 -0700

    added js and css to index,sign,login pages

--- public/js/food_index.js
+++ public/js/food_index.js
@@ -1,15 +1,14 @@
 $(() => {
 
-  const bannerBgArray = ['css/imgs/arrayimg1.jpg','css/imgs/arrayimg2.jpg',
-  'css/imgs/arrayimg3.jpg','css/imgs/arrayimg4.jpg','css/imgs/arrayimg5.jpg']
 
-
+  const bannerBgArray = ['css/imgs/arrayimg2.jpg',
+  'css/imgs/arrayimg4.jpg','css/imgs/arrayimg5.jpg']
 
 
 let i = 0
 const bannerBg = () => {
   setTimeout(function(){
-    $('.recipe-feed-nav').hide().fadeIn(2000)
+    $('.recipe-feed-nav').hide(5).fadeIn(4000)
     .css('background-image', `url(${bannerBgArray[i]})`)
     .css('background-position', 'center')
     .css('background-size', 'cover')
@@ -26,23 +25,16 @@ const bannerBg = () => {
 bannerBg()
 
 
-  // let i = 0;
-  // const bannerBg = () => {
-  //   setTimeout(function() {
-  //     $('recipe-feed-nav').css('background-image',`URL()`)
-  //     i++
-  //     if(i < bannerBgArray.length){
-  //       bannerBg()
-  //     } else {
-  //       i = 0;
-  //       bannerBg();
-  //     }
-  //   },2000)}
-  //   bannerBg()
-  // },
-  // () => {
-  //   console.log('error inside loop');
-  // }) }
+$(window).scroll(function() {
+
+  let i = $(window).scrollTop()
+
+  if(i >= 100){
+    $('#top-nav').addClass('sticky')
+  }else {
+    $('#top-nav').removeClass('sticky')
+  }
+});
 
 
 
